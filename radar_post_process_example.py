45b06358a028e77941d27764ea78ca1d37a53cba
Arien Sligar <arien.sligar@ansys.com>
2022-08-18 09:00:51 -0700

    intial commit

---
+++ radar_post_process_example.py
@@ -0,0 +1,280 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  4 14:18:56 2021
+
+@author: asligar
+"""
+
+from read_frtm import read_frtm, get_results_files
+import os
+import utils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.animation as animation
+plt.close('all')
+
+def get_script_path():
+    return os.path.dirname(os.path.realpath(__file__))
+
+
+#path = 'D:\\applications\\Walking_Radar\\walking_person_radar2.aedtresults\RadarPos1.results\\'
+path = 'D:\\applications\\Walking_Radar\\AoA_Test.aedtexport\\Sphere_Only\\Full_Time_Sweep\\'
+
+show_rd = False
+show_ra =False
+show_peaks = False
+ant_space = 0.73
+
+results_files = get_results_files(path)
+num_results = len(results_files)
+t_start = 0
+t_stop = 13
+t_sweep= np.linspace(t_start,t_stop,num=num_results)
+fps = 1/(t_sweep[1]-t_sweep[0])
+rPixels =256
+dPixels =256
+azPixels = 256
+
+#read just the header of one file to get simulation parameters like num frequency points
+#assumes data is the same for all files in the directory
+all_results = []
+all_results_ra = []
+for t_idx in range(len(t_sweep)):
+    data= read_frtm(results_files[t_idx])
+    
+    nfreq = data.nfreq
+    fc = data.freq_center
+    ntime = data.ntime
+    
+    
+    r_period = data.range_period()
+    r_resolution = data.range_resolution()
+    range_vals = np.linspace(0,r_period,num=rPixels)
+    v_period = data.velocity_period()
+    v_resolution = data.velocity_resolution()
+    vel_vals = np.linspace(-v_period/2,v_period/2,num=dPixels)
+    cpi_time = data.time_duration
+    prf = 1/data.time_delta
+    vmin = -v_period/2
+    vmax = v_period/2
+    rmax = r_period
+    fov = [-90,90]
+    
+    channel_names = data.channel_names
+    num_channels = data.num_channels
+    
+    
+    #order is if we want first index to be freq or pulse, post processing here assumes [freq][pulse] order
+    single_frame = data.load_data(order='FreqPulse') 
+    
+    
+    #################
+    # Create range doppler plot for the first channel
+    
+    data_all_channels_rd = np.zeros((num_channels,rPixels,dPixels),dtype=complex)
+    for n, ch in enumerate(channel_names):
+        data_all_channels_rd[n], range_profile, processing_fps =utils.range_doppler_map(single_frame[ch], window=False,size=(rPixels,dPixels))
+    
+    
+    #################
+    # Create AoA plot for all range bins
+    # this assumes the channels are correctly ordered, if not results will be wrong
+    # re-order channels if needed
+
+    # here I am just loading the results, which were in teh format [dict_key_channel_name][freq][pulse]
+    # into a 3D array with format [channel_number][freq][pulse]. This just loads them in in the order
+    # the keys appears. for MIMO or port names that are not sorted alphabetically you could modify this 
+    data_all_channels_fp =np.zeros((num_channels,nfreq,ntime),dtype=complex)
+    for n, ch in enumerate(channel_names):
+        data_all_channels_fp[n] = single_frame[ch]
+
+    #calculate range vs angle map across all range bins
+
+
+    all_r_angle_data,aoa_processing_fps = utils.range_angle_map(data_all_channels_fp,
+                                                                antenna_spacing_wl = ant_space,
+                                                                source_data = 'FreqPulse',
+                                                                DoA_method='bartlett',
+                                                                fov = fov, 
+                                                                out_size=(rPixels,azPixels))
+    
+    all_results_ra.append(all_r_angle_data) 
+    #range doppler for a single channel
+    #range_doppler = data_all_channels_rd[0][0:int(rPixels/2)]
+    all_results.append(data_all_channels_rd)
+  
+#all_results = 20*np.log10(np.abs(np.array(all_results)))
+all_results= np.array(all_results)
+all_results_abs = np.abs(np.array(all_results))
+
+all_results_ra = np.abs(np.array(all_results_ra))
+
+
+if show_rd:
+    # First set up the figure, the axis, and the plot element we want to animate
+    fig = plt.figure( figsize=(8,8) )
+    
+    a = 20*np.log10(np.abs(all_results[0][0]))
+
+    im = plt.imshow(a, interpolation='hamming', aspect='auto',vmin=-150,vmax=-50)
+    plt.ylabel('Range')
+    plt.xlabel('Doppler')
+    plt.title('Doppler Range')
+    def update_plot_rd(i):
+        print(t_sweep[i])
+        val = 20*np.log10(np.abs(all_results[i][0]))
+        print(np.max(val))
+        im.set_array(val)
+        return [im]
+    
+    anim = animation.FuncAnimation(
+                                    fig, 
+                                    update_plot_rd, 
+                                    frames = num_results,
+                                    interval = 1000 / fps, # in ms
+                                    )
+
+
+
+
+
+
+
+if show_ra:
+    max_ra = np.max(20*np.log10(np.abs(all_results_ra)))
+    dynamic_range = 140
+    
+    ra_to_plot1 = np.swapaxes(20*np.log10(np.abs(all_results_ra[0])),0,1)
+    
+    #normalize plot to maximum value so values stay constant for animation
+    max_of_plot1 = max_ra
+    min_of_plot1 = max_ra-dynamic_range
+    levels = np.linspace(min_of_plot1,max_of_plot1,64)
+    
+    normi = colors.Normalize(vmin=min_of_plot1, vmax=max_of_plot1)
+    
+    azimuth_vals = np.linspace(fov[0],fov[1],num=azPixels)
+    r, theta = np.meshgrid(range_vals,np.radians(azimuth_vals))
+    
+    
+    fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
+    
+    
+    ax.set_thetamin((fov[0]))
+    ax.set_thetamax((fov[1]))
+    ax.yaxis.grid(False)
+    ax.xaxis.grid(False)
+    ax.set_ylim(0,np.max(range_vals))
+    ax.set_yticks(np.arange(0,np.max(range_vals),10))
+    ax.set_title('Angle vs Range (Azimuth)')
+    #cbar = fig.colorbar(pp)
+    #plt.show()
+    
+    
+    
+    
+    def update_plot_ra(i):
+        ra_to_plot1 = np.swapaxes(20*np.log10(np.abs(all_results_ra[i])),0,1)
+        print(t_sweep[i])
+        pp = plt.contourf(theta,r,ra_to_plot1,64,cmap='rainbow',
+                levels=levels, norm=normi, extend='both')
+
+
+    anim = animation.FuncAnimation(
+                                    fig, 
+                                    update_plot_ra, 
+                                    frames = num_results,
+                                    interval = 1000 / fps, # in ms
+                                    )
+
+
+# #calculate target list
+#this can be used to plot any quanitity of detected peaks
+
+all_targets = []
+all_peaks = []
+cumulative_peaks = None
+all_xy = []
+all_range_xrange = []
+
+for idx in range(num_results):
+
+    target_list, fps_target_list, peaks = utils.create_target_list(rd_all_channels_az =all_results[idx],
+                                                        rd_all_channels_el = None,
+                                                        rngDomain = range_vals,
+                                                        velDomain = vel_vals,
+                                                        azPixels=azPixels,
+                                                        elPixels=0,
+                                                        antenna_spacing_wl=ant_space,
+                                                        radar_fov=fov,
+                                                        centerFreq=fc,
+                                                        rcs_min_detect=-10,
+                                                        min_detect_range=1,
+                                                        rel_peak_threshold = 1e-3,
+                                                        max_detections=3,
+                                                        return_cfar=True)
+    all_targets.append(target_list)
+    all_peaks.append(peaks)
+    if len(target_list)>0:
+        for tar in target_list:
+            all_xy.append([target_list[tar]['xpos'],target_list[tar]['ypos']])
+            all_range_xrange.append([target_list[tar]['range'],target_list[tar]['azimuth']])
+    if cumulative_peaks is None:
+        cumulative_peaks =peaks
+    else:
+        cumulative_peaks+=peaks
+
+
+
+if show_peaks:
+    
+    # First set up the figure, the axis, and the plot element we want to animate
+    fig = plt.figure( figsize=(8,8) )
+    
+    a = all_peaks[0]
+    im = plt.imshow(a, interpolation='hamming', aspect='auto')
+    plt.ylabel('Range')
+    plt.xlabel('Cross Range')
+    plt.title('Doppler Range')
+    def update_plot_peak(i):
+        print(t_sweep[i])
+        im.set_array(all_peaks[i])
+        return [im]
+    
+    anim = animation.FuncAnimation(
+                                    fig, 
+                                    update_plot_peak, 
+                                    frames = num_results,
+                                    interval = 1000 / fps, # in ms
+                                    )
+    
+    
+
+
+    
+
+
+# all_range_xrange = np.array(all_range_xrange)
+# all_r = all_range_xrange[:,0]
+# all_theta = all_range_xrange[:,1]*np.pi/180
+# colors = np.zeros(len(all_theta))
+# fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
+
+
+# ax.set_thetamin((fov[0]))
+# ax.set_thetamax((fov[1]))
+# ax.yaxis.grid(False)
+# ax.xaxis.grid(False)
+# ax.set_ylim(0,np.max(range_vals))
+# ax.set_yticks(np.arange(0,np.max(range_vals),10))
+# ax.set_title('Angle vs Range (Azimuth)')
+
+# all_r_ideal = all_range_xrange_ideal[:,0]
+# all_theta_ideal = all_range_xrange_ideal[:,1]*np.pi/180
+# colors_ideal= np.ones(len(all_theta_ideal))
+# colors2 = np.hstack((colors_ideal,colors))
+# all_theta2 = np.hstack((all_theta_ideal,all_theta))
+# all_r2 = np.hstack((all_r_ideal,all_r))
+# c = ax.scatter(all_theta2, all_r2,c=colors2)
+
---
+++ read_frtm.py
@@ -0,0 +1,255 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 08:41:28 2020
+
+@author: asligar
+"""
+
+import numpy as np
+import csv
+import glob
+import os
+
+def get_results_files(path,var_name='time_var'):
+
+    index_file = glob.glob(path + '\\*.csv')[0]
+    
+    
+    all_sol_files = glob.glob(path + '\\*.frtm')
+    path, file_name = os.path.split(all_sol_files[0])
+    file_name_prefix = file_name.split("_DV")[0]
+    
+    
+    var_IDS = []
+    var_name = 'time_var'
+    var_vals = []
+    with open(index_file, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                #print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            if row["Var_ID"] not in var_IDS:
+                var_IDS.append(row["Var_ID"])
+                if 's' in row[var_name]:
+                    val = float(row[var_name].replace("s",""))
+                else:
+                    val = float(row[var_name])
+                var_vals.append(val)
+            #print(f'\t{row["name"]} works in the {row["department"]} department, and was born in {row["birthday month"]}.')
+            line_count += 1
+
+    
+    variation_var_IDS = sorted(zip(var_vals,var_IDS))
+
+    all_frtm =[]  
+    for var_val, id_num in variation_var_IDS:
+        #all_frtm[var_val]=f'{path}/{file_name_prefix}_DV{id_num}.frtm'
+        all_frtm.append(f'{path}/{file_name_prefix}_DV{id_num}.frtm')
+        
+    print(f'Variations Founds: {len(all_frtm)}.')
+    return all_frtm
+
+class read_frtm(object):
+    """
+    reads frtm data output in .transient file
+    some of the loops are hardcoded, in the future I will read them
+    """
+    def __init__(self,filepath):
+        """
+        reads frtm file from path
+        """
+
+        string_to_stop_reading_header = '@ BeginData'
+        header = []    
+        with open(filepath, "rb") as binary_file:
+            line = binary_file.readline()
+            line_str = line.decode('ascii')
+            while string_to_stop_reading_header not in line_str:
+                header.append(line_str)
+                line = binary_file.readline()
+                line_str = line.decode('ascii')
+                if line_str.replace(" ","") =="":
+                    pass
+                elif 'DlxCdVersion' in line_str:
+                    dlxcd_vers_line = line_str
+                    vers = dlxcd_vers_line.split("=")
+                    self.dlxcd_vers = vers
+                elif '@ RowCount' in line_str:
+                    c = line_str.split("=")
+                    c = c[1].replace("\n","").replace("\"","").replace(" ","")
+                    self.row_count =  int(c)
+
+                elif '@ ColumnCount' in line_str:
+                    c = line_str.split("=")
+                    c = c[1].replace("\n","").replace("\"","").replace(" ","")
+                    self.col_count =  int(c)
+                elif '@ ColHead1' in line_str:
+                    c = line_str.split("=")
+
+                    c = c[1].split(" ")
+                    c = [i for i in c if i]
+                    c = c[0].replace("\n","").replace("\"","").replace(" ","")
+                    self.col_header1 =  c
+                elif '@ ColHead2' in line_str:
+                    c = line_str.split("=")
+
+                    c = c[1].split(" ")
+                    c = [i for i in c if i]
+                    c = c[0].replace("\n","").replace("\"","").replace(" ","")
+                    self.col_header2 =  c
+                elif '@ BinaryRecordLength ' in line_str:
+                    bin_record_length_line = line_str  
+                    c = bin_record_length_line.split("=")
+                    self.bin_record_lenth = c[1]
+                elif '@ BinaryStartByte ' in line_str:
+                    c = line_str.split("=")
+                    c = c[1].replace("\n","").replace("\"","").replace(" ","")
+                    self.binary_start_byte = int(c)
+                elif '@ BinaryRecordSchema ' in line_str:
+                    self.bin_byte_type_line = line_str
+                elif '@ RadarWaveform ' in line_str:
+                    radarwaveform_line = line_str
+                    rw = radarwaveform_line.split("=")
+                    self.radar_waveform = rw[1].replace("\n","").replace("\"","").replace(" ","" )
+                elif '@ RadarChannels ' in line_str:
+                    radarchannels_line = line_str
+                    rc = radarchannels_line.split("=")
+                    self.radarchannels =  rc[1].replace("\n","").replace("\"","").replace(" ","" )
+                elif '@ TimeSteps ' in line_str:
+                    time_steps_line = line_str
+                    c = time_steps_line.split("=")
+                    c = c[1].split(" ")
+                    c = [i for i in c if i]
+                    self.time_start= float(c[0].replace("\"",""))
+                    self.time_stop= float(c[1].replace("\"",""))  
+                    c = time_steps_line.split("=")
+                    c = c[1].split(" ")
+                    c = [i for i in c if i]
+                    self.ntime= int(c[2].replace("\"",""))+1
+                    self.time_sweep = np.linspace(self.time_start,self.time_stop,num=self.ntime)
+                    self.time_delta= self.time_sweep[1]-self.time_sweep[0]
+                    self.time_duration =  self.time_sweep[-1]-self.time_sweep[0]
+                elif '@ FreqDomainType ' in line_str:
+                    freq_dom_type_line = line_str
+                    self.freq_dom_type = freq_dom_type_line.split("=")[1]
+                elif '@ FreqSweep ' in line_str:
+                    freq_sweep_line = line_str  
+                    c = freq_sweep_line.split("=")
+                    c = c[1].replace("\"","")
+                    c = c.lstrip()
+                    c = c.rstrip()
+                    c = c.split(" ")
+                    c = [i for i in c if i]
+                    self.freq_start =  float(c[0])
+                    self.freq_stop = float(c[1])
+                    self.nfreq = int(c[2].replace("\"",""))+1
+                    if (self.radar_waveform == 'CS-FMCW' and self.radarchannels == 'I'):
+                        self.nfreq = int(self.nfreq/2)
+                    self.freq_sweep = np.linspace(self.freq_start,self.freq_stop,num=self.nfreq)
+                    self.freq_delta = self.freq_sweep[1]-self.freq_sweep[0]
+                    self.freq_bandwidth = self.freq_sweep[-1]-self.freq_sweep[0]
+                    center_index = int(self.nfreq/2)
+                    self.freq_center = float(self.freq_sweep[center_index])
+                elif '@ AntennaNames ' in line_str:
+                    ant_names_line = line_str
+                    c = ant_names_line.split("=")
+                    c = c[1].replace("\n","")
+                    c = c.replace("\"","").replace(" ","" )
+                    an = c.split(';')
+                    self.antenna_names = an
+                elif '@ CouplingCombos '  in line_str:
+                    coupling_combos_line = line_str
+                    c = coupling_combos_line.replace("\"","")
+                    c = c.replace("\n","")
+                    c = c.split("=")[1]
+                    c = c.split(" ")
+                    c = [i for i in c if i]
+                    self.num_channels = int(c[0])
+                    self.coupling_combos = c[1].split(";")
+            self.filepath = filepath
+       
+        #this is the order in the frtm file
+           
+        self.channel_names = []
+        for each in self.coupling_combos:
+            index_values = each.split(',')
+            rx_idx =index_values[0]
+            tx_idx =index_values[1]
+            if ":" in rx_idx:
+                rx_idx = int(rx_idx.split(':')[0])
+            if ":" in tx_idx:
+                tx_idx = int(tx_idx.split(':')[0])
+            tx_idx = int(tx_idx)-1
+            rx_idx = int(rx_idx)-1
+            self.channel_names.append(self.antenna_names[rx_idx] + ":"+ self.antenna_names[tx_idx] )
+   
+        if self.col_count==2:
+            dt = np.dtype([(self.col_header1, float), (self.col_header2, float)])
+        else:
+            dt = np.dtype([(self.col_header1, float)])   
+        raw_data = np.fromfile(filepath, dtype=dt,offset =self.binary_start_byte )
+       
+
+        self.all_data = {}
+        
+        #cdat_real = np.moveaxis(cdat_real,-1,0)
+        if self.col_count==2:
+            cdat_real = np.reshape(raw_data[self.col_header1],(self.num_channels,self.ntime,self.nfreq))
+            cdat_imag = np.reshape(raw_data[self.col_header2],(self.num_channels,self.ntime,self.nfreq))
+        #cdat_imag = np.moveaxis(cdat_imag,-1,0)
+            for n, ch in enumerate(self.channel_names):
+                self.all_data[ch] = cdat_real[n]+cdat_imag[n]*1j
+        else:
+            cdat_real = np.reshape(raw_data[self.col_header1],(self.num_channels,self.ntime,int(self.nfreq*2))) #fmcw I channel
+            for n, ch in enumerate(self.channel_names):
+                temp = cdat_real[n]
+                #temp2 = temp.T[0::2] #every other sample for fmcw I channel only
+                #temp2 = temp.T[0:80]
+                self.all_data[ch] = temp
+                
+
+
+
+    def range_period(self):
+        rr = self.range_resolution()
+        max_range = rr*self.nfreq
+        return max_range
+    def range_resolution(self):
+        bw = self.freq_bandwidth
+        rr = 299792458.0/2/bw
+        return rr
+    def velocity_resolution(self):
+        fc = self.freq_center
+        tpt= self.time_duration
+        vr = 299792458.0/(2*fc*tpt)
+        return vr
+    def velocity_period(self):
+        vr = self.velocity_resolution()
+        np = self.ntime
+        vp = np*vr
+        return vp
+
+    def load_data(self,order='FreqPulse'):
+        '''
+
+        Parameters
+        ----------
+        order : TYPE, optional
+            DESCRIPTION. order is either 'FreqPulse' or 'PulseFreq'. this is
+            the index order of the array, [numFreq][numPulse] or [numPulse][numFreq]
+            many of the post processing depend on this order so just choose accordingly
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        '''
+        if order.lower()=='freqpulse':
+            for ch in self.all_data.keys():
+                self.all_data[ch] = self.all_data[ch].T
+            return self.all_data
+        else: #this is how the data is read in with [pulse][freq] order
+            return self.all_data
